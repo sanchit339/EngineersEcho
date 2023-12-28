a6a9f033035ffe00afc1ed46d0d190500de32a69
Sanchit Ingale <sanchitingale09@gmail.com>
2023-12-28 15:17:12 +0530

    Profile and auth Done

--- EngineersEcho/urls.py
+++ EngineersEcho/urls.py
@@ -17,11 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_view
+from django.contrib.auth import views as auth_view
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 
     # user Auth
-    path('register/', user_view.register, name='register')
+    path('register/', user_view.register, name='register'),
+    path('profile/', user_view.profile, name='profile'),
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
+
+# its used only for Development phase where you want to serve static media
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,18 +1,42 @@
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid(): #pass the parameter of the username and password
+        if form.is_valid():  # pass the parameter of the username and password
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created Succesfully for {username}!')
+            messages.success(request, f'Account Created Succesfully for {username}! Login In Now')
             return redirect('blog-home')
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+    # we want the current details of the user in the form field (html placeholder)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        # here we want instance of the user profile
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request , f'Your Profile is Updated!')
+            redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
