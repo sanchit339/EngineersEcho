e6fe711b3e815d9506eee0e30b1b2c2c206a38bf
Ubuntu <ubuntu@ip-172-31-87-2.ec2.internal>
2023-12-31 11:47:24 +0000

    allowed Host changes

--- EngineersEcho/settings.py
+++ EngineersEcho/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w4umm)1e=jf5j&8f&hc@sjv&=2r68&ij992b*tp=c_4sn$1k^r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.203.171.186', 'localhost', '127.0.0.1']
 
 # Application definition
 
